b9fb563fa3a5b330d1f920102af808dcf8dbad86
TakteS <takt96@gmail.com>
2013-08-05 00:57:33 +1000

    Views fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Bebas::Application.routes.draw do
   devise_for :users
-  root :to => 'guestbook/messages#index'
+  root :to => 'index#startpage'
   get  '/users' => 'users#index'
   get '/profile/:user' => 'users#show'
-  get '/' => 'index#startpage'
   get '/news' => 'news/articles#index'
   get '/guestbook' => 'guestbook/messages#index' 
   post '/guestbook/say' => 'guestbook/messages#say'
